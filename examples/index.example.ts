2e2b9cb0e6644fc8a22ff918ec1ad54dde7a7057
JawadAhbab <jawad.ahbab.turna@gmail.com>
2024-03-20 07:49:09 +0600

    🔥 CheckContrast

--- examples/index.example.ts
+++ examples/index.example.ts
@@ -1,3 +1,9 @@
+import { contrastColor } from 'contrast-color'
 import { isDark } from '../src/index'
 
-console.log(isDark('black'))
+console.log(
+  contrastColor({
+    bgColor: '#00000',
+    threshold: 165,
+  })
+)
