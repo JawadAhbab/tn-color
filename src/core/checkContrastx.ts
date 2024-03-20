2947d501ff77354490c1dfca57b02a35296d1cfe
JawadAhbab <jawad.ahbab.turna@gmail.com>
2024-03-20 07:51:53 +0600

    🔥 CheckContrast

---
+++ src/core/checkContrastx.ts
@@ -0,0 +1,18 @@
+export class CheckContrast {}
+
+// const isLevelAA = (bg: string, fg: string, fs: number) => {
+//   var result = check(bg, fg, fs)
+//   // return result.WCAG_AA
+// }
+// const isLevelAAA = (bg: string, fg: string, fs: number) => {
+//   var result = check(bg, fg, fs)
+//   // return result.WCAG_AAA
+// }
+
+// const check = (colorA: string, colorB: string, fontSize: number) => {
+//   if (!colorA || !colorB) return false
+//   // var l1 = hexToLuminance(colorA)
+//   // var l2 = hexToLuminance(colorB)
+//   // var contrastRatio = getContrastRatio(l1, l2)
+//   // return verifyContrastRatio(contrastRatio)
+// }
